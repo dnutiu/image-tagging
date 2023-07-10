bc2b0cfa6915490fd58a742b9d12bed674a48233
dnutiu <denis.nutiu@gmail.com>
2023-07-10 20:50:44 +0300

    Add demo code for using dynamic categories

---
+++ ImageTagger.ConsoleTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using ImageTagger.Core;
+
+namespace ImageTagger.ConsoleTest;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+
+        
+        const string modelPath = @"C:\Users\nutiu\RiderProjects\ImageTagger\data\resnet50.onnx";
+        const string labelsPath = @"C:\Users\nutiu\RiderProjects\ImageTagger\data\categories.txt";
+        var modelPrediction = new ModelPrediction(modelPath, labelsPath);
+
+        for (var i = 1; i <= 12; i++)
+        {
+            Console.WriteLine($"Predicting... {i}.jpg");
+            var prediction = modelPrediction.PredictTags($@"C:\Users\nutiu\Documents\i\{i}.jpg");
+
+            foreach (var pred in prediction.Where(prediction => prediction.Confidence > -0.5))
+            {
+                Console.WriteLine(pred.Label, pred.Confidence);
+            }
+            
+        }
+    }
+}
