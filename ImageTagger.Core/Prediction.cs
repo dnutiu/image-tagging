c041ac55a130ff9d8fc6b6481fccbecf1d69d8dc
dnutiu <denis.nutiu@gmail.com>
2023-07-18 23:17:56 +0300

    Add docstring to Prediction.cs

--- ImageTagger.Core/Prediction.cs
+++ ImageTagger.Core/Prediction.cs
@@ -1,3 +1,8 @@
 ﻿namespace ImageTagger.Core;
 
+/// <summary>
+/// Prediction is a record that represents a prediction made by a model.
+/// </summary>
+/// <param name="Label">The prediction label / category.</param>
+/// <param name="Confidence">The confidence level of the prediction. Higher is better.</param>
 public record Prediction(string Label, float Confidence);
